53947441c6b79ab7b832a4275b3b6e8e4b71e6ea
Anjali Gunduka <anjalig@nbos.vinisoft.com>
2021-09-17 14:13:58 +0530

    WithoutBean

--- src/main/java/com/springs/Example/SpringAnnotationss/AppConfig.java
+++ src/main/java/com/springs/Example/SpringAnnotationss/AppConfig.java
@@ -1,19 +1,12 @@
 package com.springs.Example.SpringAnnotationss;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages="com.springs.Example.SpringAnnotationss")
 public class AppConfig {
-	@Bean
- public Samsung getPhone()
- {
-	 return new Samsung();
- }
-	@Bean
-	 public MobileProcessor getProcessor()
-	 {
-		 return new  Snapdragon();
-	 }
-	
+
+
 }
